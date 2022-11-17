d1606a5e9c6984f610d2264d71a52650d924a080
Kop1tes <prsasha0@gmail.com>
2022-11-17 15:08:00 +0200

    add first changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// console.log(createGallery(galleryItems));
+
+const gallery = document.querySelector('.gallery');
+const imgGallery = createGallery(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', imgGallery);
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `
+    }).join('');
+}
+
+gallery.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery')) {
+        return;
+    }
+
+    const instance = basicLightbox.create(
+	document.querySelector('.gallery')
+)
+    
+    console.log(event.target);
+}
